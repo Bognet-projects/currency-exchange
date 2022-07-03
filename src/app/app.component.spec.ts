0cc86e69839a196247497ec6e48bc746958574b6
Balázs Bognár <bognar.balazs@bognet.hu>
2022-07-03 12:14:36 +0200

    change HttpClientTestingModule import to properly mock ApiService

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {MatCardModule} from "@angular/material/card";
 import {FormComponent} from "./components/form/form.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
+import {ApiService} from "./services/api.service";
+import {of} from "rxjs";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    const apiServiceSpy = jasmine.createSpyObj<ApiService>(['getExchangeRate'])
+    apiServiceSpy.getExchangeRate.and.returnValue(of(0))
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         FormComponent
       ],
-      imports:[HttpClientTestingModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule]
+      imports:[MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule],
+      providers: [{provide: ApiService, useValue: apiServiceSpy}]
     }).compileComponents();
   });
 
